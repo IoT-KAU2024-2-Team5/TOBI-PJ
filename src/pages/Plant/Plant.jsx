4814025163cbb6662561269c2bf0990453bc3b61
sumhillj <sumhillj@gmail.com>
2024-12-02 21:26:02 +0900

    feat: added extra view

--- src/pages/Plant/Plant.jsx
+++ src/pages/Plant/Plant.jsx
@@ -4,6 +4,9 @@ import { usePlantContext } from '../../contexts/PlantContext.jsx';
 import Plant1 from '../../assets/산세베리아.svg';
 import Plant2 from '../../assets/스킨답서스.svg';
 import Plant3 from '../../assets/테이블야자.svg';
+import Tip1 from '../../assets/Tip1.svg';
+import Tip2 from '../../assets/Tip2.svg';
+import Tip3 from '../../assets/Tip3.svg';
 import Control from '../../components/Control/Control';
 import Status from '../../components/Status/Status';
 
@@ -24,10 +27,16 @@ function Plant() {
     테이블야자: Plant3,
   };
 
+  const tipImages = {
+    산세베리아: Tip1,
+    스킨답서스: Tip2,
+    테이블야자: Tip3,
+  };
+
   const plantImage = plantImages[id];
+  const tipImage = tipImages[id];
 
   const calculateDateCount = () => {
-    // 기본 날짜 차이는 0일로 설정
     const creationDate = new Date();
     const currentDate = new Date();
     const timeDiff = currentDate - creationDate;
@@ -44,6 +53,7 @@ function Plant() {
     '오늘 하루는 어떠셨나요?',
   ];
   const [messageIndex, setMessageIndex] = useState(0);
+  const [isTipCardVisible, setTipCardVisible] = useState(false);
 
   const handlePlantClick = () => {
     setMessages((prev) => {
@@ -58,6 +68,10 @@ function Plant() {
     setMessageIndex((prevIndex) => (prevIndex + 1) % messageTexts.length);
   };
 
+  const handleTipClick = () => {
+    setTipCardVisible((prevVisible) => !prevVisible);
+  };
+
   return (
     <S.PlantWrapper>
       <Status ledValue={led} plant={id} username={plantName} />
@@ -69,13 +83,14 @@ function Plant() {
         <S.PlantImage src={plantImage} alt={id} onClick={handlePlantClick} />
       </S.PotContainer>
       <S.IconsContainer>
-        <S.TipContainer>
+        <S.TipContainer onClick={handleTipClick}>
           <S.TipIcon /> Tip !
         </S.TipContainer>
         <S.DeathContainer>
           <S.DeathIcon /> 식물이 시들었어요
         </S.DeathContainer>
       </S.IconsContainer>
+      {isTipCardVisible && <S.TipCard tipImage={tipImage} />}
       <Control ledValue={led} setLedValue={setLed} />
       <S.MessageContainer>
         {messages.map((message) => (
